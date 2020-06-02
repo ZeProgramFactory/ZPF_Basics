776ce0e434412df3028cebb49f22bd4b5a992f42
Michael <Michael@ZPF.fr>
2020-06-02 23:09:15 +0200

    I18n_Interface

---
+++ Sources/Models/I18n_Interface.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ZPF
+{
+   public interface I18n_Interface
+   {
+      string Key { get; set; }
+      string Value { get; set; }
+      string Language { get; set; }
+      string App { get; set; }
+      string Source { get; set; }
+      bool? Missing { get; set; }
+      bool? Validated { get; set; }
+   }
+}
