a731fa9be3f8d3464e412de94c9bf439f843cc45
Michael <Michael@ZPF.fr>
2020-08-21 21:26:51 +0200

    Excel: HAlignments.Default / VAlignments.Default

--- Sources/Models/Layout_Basics.cs
+++ Sources/Models/Layout_Basics.cs
@@ -11,8 +11,16 @@ namespace ZPF
    public enum Orientations { Portrait, Landscape }
    public enum PageSizes { Custom, A4 }
 
-   public enum HAlignments { Left, Center, Right, Justify, None }
-   public enum VAlignments { Top, Center, Bottom, None }
+   public enum HAlignments { Default, Left, Center, Right, Justify,
+      /// <summary>
+      /// (Excel) will center text along cells with same style.
+      CenterContinuous
+   }
+   public enum VAlignments { Default, Top, Center, Bottom }
+
+   //Excel public enum HAlignments { Default, Left, Center, Right, CenterContinuous };
+   //Excel public enum VAlignments { Default, Top, Center, Bottom };
+
    public enum LineStyles { None, Hair, Thin, Medium }
 
    // - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - 
@@ -45,7 +53,7 @@ namespace ZPF
       public int NbChars { get; set; } = -1;
       public double Width { get; set; }
 
-      public HAlignments HAlignment { get; set; } = HAlignments.None;
+      public HAlignments HAlignment { get; set; } = HAlignments.Default;
       //public NumFmtIds NumFmtId { get; set; } = NumFmtIds.none;
 
       // - - -  - - - 
