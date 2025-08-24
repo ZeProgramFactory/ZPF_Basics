46fac72fa5c789f2ead8ab2d710da2e487b03472
Michael <Michael@ZPF.fr>
2025-08-24 11:40:01 +0200

    Update IVersionInfo interface and package reference
    - Added XML documentation for IVersionInfo properties.
    - Changed sVersion and BuildOn to read-write properties.
    - Introduced a new read-only property, RevisionNumber.
    - Updated System.Text.Json package reference to version 9.0.8.

--- Sources/Models/IVersionInfo.cs
+++ Sources/Models/IVersionInfo.cs
@@ -2,11 +2,25 @@
 
 namespace ZPF;
 
+/// <summary>
+///  Represents the version information of an build
+/// </summary>
 public interface IVersionInfo
 {
-   public string sVersion { get; }
+   /// <summary>
+   /// Represents the version number as string
+   /// </summary>
+   public string sVersion { get; set; }
+
+   /// <summary>
+   /// Represents the version number
+   /// </summary>
    public Version Version { get; }
 
-   public string BuildOn { get; }
+   /// <summary>
+   /// Represents the build date part of build timestamp as string
+   /// </summary>
+   public string BuildOn { get; set; }
+
    public int RevisionNumber { get; }
 }
